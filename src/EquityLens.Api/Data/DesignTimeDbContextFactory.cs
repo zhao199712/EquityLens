7f7947ae70a83c3391f3b0124faca7a5d922f73a
zhao199712 <ymsh20220@gmail.com>
2026-06-09 20:00:40 +0800

    fix: correct DB port and credentials in DesignTimeDbContextFactory

--- src/EquityLens.Api/Data/DesignTimeDbContextFactory.cs
+++ src/EquityLens.Api/Data/DesignTimeDbContextFactory.cs
@@ -10,7 +10,7 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EquityLens
     {
         var options = new DbContextOptionsBuilder<EquityLensDbContext>()
             .UseNpgsql(
-                "Host=localhost:5433;Database=equitylens;Username=equitylens;Password=equitylens",
+                "Host=localhost:5432;Database=equitylens;Username=ymsh20220;Password=ymsh20220",
                 o => o.UseVector())
             .Options;
         return new EquityLensDbContext(options);
